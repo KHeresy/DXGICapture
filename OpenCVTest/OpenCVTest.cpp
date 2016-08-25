fbd551c956aa26d87c6ef259d665dc472a874d1c
Heresy <kheresy@gmail.com>
2016-08-25 11:26:44 +0800

    Update OpenCV test
    1. update to OpenCV 3.1
    2. Use OpenCL for OpenCV

--- OpenCVTest/OpenCVTest.cpp
+++ OpenCVTest/OpenCVTest.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #include <opencv2/core.hpp>
+#include <opencv2/core/ocl.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 
@@ -22,20 +23,23 @@ int main(int argc, char** argv)
 
 	printf("dwWidth=%d dwHeight=%d\n", dwWidth, dwHeight);
 
+	cv::ocl::setUseOpenCL(true);
 	cv::Mat mScreen(dwHeight, dwWidth, CV_8UC4);
+	cv::UMat mShowImg;
 
 	cv::namedWindow("Screen");
 	while (true)
 	{
 		if (mDXGIManager.GetOutputBits(mScreen.data, rcDim) == S_OK)
 		{
-			cv::Mat mShowImg;
-			cv::resize(mScreen, mShowImg, cv::Size(mScreen.cols / 3, mScreen.rows / 3));
+			cv::resize(mScreen, mShowImg, cv::Size(mScreen.cols / 2, mScreen.rows / 2));
 			cv::imshow("Screen", mShowImg);
 		}
-		if (cv::waitKey(30) == 'q')
+		if (cv::waitKey(5) == 'q')
 		{
 			break;
 		}
 	}
+
+	CoUninitialize();
 }
